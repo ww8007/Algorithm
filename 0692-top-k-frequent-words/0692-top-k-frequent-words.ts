69591acc75249345d9545dded91f6dac5a8aa8f9
장동현 <54137044+ww8007@users.noreply.github.com>
2023-04-12 22:07:40 +0900

    Time: 80 ms (68.29%), Space: 47.9 MB (54.27%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.ts
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.ts
@@ -8,9 +8,9 @@ function topKFrequent(words: string[], k: number): string[] {
     
     arr = arr.sort((a, b) => {
     if (a[1] === b[1]) {
-        return a[0].localeCompare(b[0]); // 알파벳순으로 정렬
+        return a[0].localeCompare(b[0]); 
     }
-        return b[1] - a[1]; // 빈도수를 기준으로 내림차순 정렬
+        return b[1] - a[1];
     }).slice(0, k);
     
     return arr.map(([k, v]) => k);
