5a041cb3c91e084ae9f457a5f8a376729394de84
장동현 <54137044+ww8007@users.noreply.github.com>
2023-04-13 12:49:46 +0900

    Time: 88 ms (28.74%), Space: 45.5 MB (39.49%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.ts
+++ 0205-isomorphic-strings/0205-isomorphic-strings.ts
@@ -1,12 +1,10 @@
 function isIsomorphic(s: string, t: string): boolean {
-    if (s.length !== t.length) return false;
-    const fmap = new Map();
-    const smap = new Map();
-    for (let i = 0; i < s.length; i++) {
-        if (fmap.has(s[i]) && fmap.get(s[i]) !== t[i]) return false;
-        if (smap.has(t[i]) && smap.get(t[i]) !== s[i]) return false;
-        fmap.set(s[i], t[i]);
-        smap.set(t[i], s[i]);
-    }
-    return true;
-};
+  if (s.length !== t.length) return false;
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i]) && map.get(s[i]) !== t[i]) return false;
+    if (!map.has(s[i]) && [...map.values()].includes(t[i])) return false;
+    map.set(s[i], t[i]);
+  }
+  return true;
+}
