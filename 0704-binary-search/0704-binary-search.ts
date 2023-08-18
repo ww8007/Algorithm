ef5c6a937170d37f93edf7bed891617848afd114
장동현 <54137044+ww8007@users.noreply.github.com>
2023-08-19 03:28:53 +0900

    Time: 62 ms (67.20%), Space: 45.9 MB (59.53%) - LeetHub

--- 0704-binary-search/0704-binary-search.ts
+++ 0704-binary-search/0704-binary-search.ts
@@ -2,12 +2,12 @@ function search(nums: number[], target: number): number {
     let lp = 0;
     let rp = nums.length - 1;
     
-    
     while (lp <= rp) {
         const mid = Math.floor((lp + rp) / 2);
-        if (nums[mid] === target) return mid;
-        else if (nums[mid] < target) lp = mid + 1;
-        else rp = mid - 1;
+        const currentNum = nums[mid]
+        if (currentNum < target) lp = mid + 1;
+        else if (currentNum > target) rp = mid - 1;
+        else return mid;
     }
     
     return -1;
