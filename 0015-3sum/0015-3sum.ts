53855cdee91b540bdf8e7b68b7fe7a259cfc02e8
장동현 <54137044+ww8007@users.noreply.github.com>
2023-08-21 02:33:33 +0900

    Time: 161 ms (83.27%), Space: 59.4 MB (51.84%) - LeetHub

--- 0015-3sum/0015-3sum.ts
+++ 0015-3sum/0015-3sum.ts
@@ -1,36 +1,34 @@
 function threeSum(nums: number[]): number[][] {
     nums.sort((a, b) => a - b);
-    const len = nums.length - 1;
-    const result = [];
-
-    for (let i = 0; i < len - 1; i++) {
-        // 중복된 숫자는 건너뛴다
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
-
+    
+    const ans = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        // 중복된 숫자 건너뛰기
+        if (i > 0 && nums[i] === nums[i-1]) continue;
+        
         let lp = i + 1;
-        let rp = len;
-
+        let rp = nums.length - 1;
+        
         while (lp < rp) {
             const sum = nums[i] + nums[lp] + nums[rp];
-
+            
             if (sum === 0) {
-                result.push([nums[i], nums[lp], nums[rp]]);
-
-                // 중복된 숫자는 건너뛴다
-                while (lp < rp && nums[lp] === nums[lp + 1]) lp++;
-                while (lp < rp && nums[rp] === nums[rp - 1]) rp--;
-
+                ans.push([nums[i], nums[lp], nums[rp]])
+                
+                 // 중복된 숫자 건너뛰기
+                while(lp < rp && nums[lp] === nums[lp+1]) lp++;
+                while(lp < rp && nums[rp] === nums[rp-1]) rp--;
+            
                 lp++;
                 rp--;
-            } else if (sum < 0) {
-                lp++;
+            } else if (sum > 0) {
+                rp--
             } else {
-                rp--;
+                lp++;
             }
         }
     }
-
-    return result;
-}
-
-
+    
+    return ans;
+};
