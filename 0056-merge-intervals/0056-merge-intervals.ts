62f64fcab055212078a3d2f14f51b5fdca769813
장동현 <54137044+ww8007@users.noreply.github.com>
2023-08-20 12:32:45 +0900

    Time: 106 ms (68.99%), Space: 52.7 MB (19.73%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.ts
+++ 0056-merge-intervals/0056-merge-intervals.ts
@@ -1,19 +1,20 @@
 function merge(intervals: number[][]): number[][] {
-    intervals.sort((a, b) => a[0] - b[0])
-
-    let ans = [intervals[0]];
+    intervals.sort((a, b) => a[0] - b[0]);
+    
+    const ans = [intervals[0]];
+    
     let cur = ans[0];
     
-    for (let i = 1; i < intervals.length; i++) {
+    for (let i = 0; i < intervals.length; i++) {
+        
         const [min, max] = intervals[i];
         if (cur[1] >= min) {
-            cur[1] = Math.max(cur[1], max);
+            cur[1] = Math.max(max, cur[1]);
         } else {
             cur = [min, max];
             ans.push(cur);
         }
-        
     }
-
+    
     return ans;
-}
+};
