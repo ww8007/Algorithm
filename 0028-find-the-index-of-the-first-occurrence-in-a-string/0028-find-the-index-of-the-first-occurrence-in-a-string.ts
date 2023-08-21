6563da30cedb199fab07c5d3c9fb40269f6cb517
장동현 <54137044+ww8007@users.noreply.github.com>
2023-08-22 04:25:00 +0900

    Time: 60 ms (52.61%), Space: 43.1 MB (44.16%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.ts
@@ -2,11 +2,10 @@ function strStr(haystack: string, needle: string): number {
     const set = new Set();
     set.add(needle);
     
-    console.log(set);
-    
-    for(let i = 0; i < haystack.length; i++) {
-        const parse = haystack.substring(i, i + needle.length);
-        if (set.has(parse)) return i;
+    for (let i = 0; i < haystack.length; i++) {
+        const slice = haystack.slice(i, i + needle.length);
+        if (set.has(slice)) return i;
     }
+    
     return -1;
 };
