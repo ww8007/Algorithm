250bef91c4f82026ead0cfd1ab507e71fee61a26
장동현 <54137044+ww8007@users.noreply.github.com>
2023-10-10 03:00:42 +0900

    Time: 101 ms (6.20%), Space: 53.8 MB (11.42%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.ts
@@ -0,0 +1,14 @@
+function maxSubArray(nums: number[]): number {
+    let sIdx = 0;
+    
+    const len = nums.length;
+    
+    let dp = Array.from({ length : len }, () => 0);
+    dp[0] = nums[0];
+    
+    for (let i = 1; i < len; i++) {
+        dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+    }
+    
+    return dp.reduce((a, b) => Math.max(a, b), -Number.MAX_SAFE_INTEGER);
+};
